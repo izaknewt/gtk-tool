2abb788a6e676bade990674c081827abe89eeeff
izak newton <newton.izak@tuanota.com>
2020-12-01 22:48:29 +0100

    added button

--- main.c
+++ main.c
@@ -9,11 +9,16 @@
 
 int main(int argc, char *argv[]) {
     GtkWidget *window;
+    GtkWidget *button;
+
     gtk_init (&argc, &argv);
 
+    button = gtk_button_new_with_label("test");
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW (window), "Hello World");
-    gtk_widget_show (window);
+    gtk_window_set_default_size(window, 400, 600);
+    gtk_container_add(GTK_CONTAINER(window),button);
+    gtk_widget_show_all(window);
     gtk_main();
     return 0;
 }
